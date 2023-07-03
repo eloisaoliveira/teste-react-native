6dfa2a97605938006dcb8632417cd699171554ee
eloisaoliveira <eloisa.oliveirasantos@gmail.com>
2023-07-03 17:25:08 -0300

    add Routes

--- App.tsx
+++ App.tsx
@@ -2,10 +2,10 @@ import { StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base'
 import { useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto'
 
+import { Routes } from './src/routes';
+
 import { THEME } from './src/theme'
 import { Loading } from '@components/Loading';
-import { SignIn } from '@screens/Signin';
-import { SignUp } from '@screens/SignUp';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold})
@@ -16,7 +16,7 @@ export default function App() {
         backgroundColor='transparent'
         translucent
       />
-      {fontsLoaded ? <SignUp/> : <Loading />}
+      {fontsLoaded ? <Routes/> : <Loading />}
     </NativeBaseProvider>
   );
 }
